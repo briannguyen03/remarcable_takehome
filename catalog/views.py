28e395cbb2b576be1b34743fcbc4615bc7246af8
Brian <nguyen.brian1403@gmail.com>
2026-03-21 20:03:56 -0700

    implemented search and filtering logic

---
+++ catalog/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.db.models import Q
+from django.http import HttpResponse
+
+from .models import Movie, Category, Tag
+
+# Create your views here.
+
+
+def movie_list(request):
+    """
+    Movie search and filtering logic. Retreives all movies, categories, and tags, 
+    then matches them to the search_querry, or filters applied using the GET request.
+
+    aggregate all of the fields: movies, categories, and tags into a dictionary 'context' for the html to parse
+    """
+    movies = Movie.objects.all()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+
+    search_querry = request.GET.get('search')
+    category_filter = request.GET.get('category')
+    tag_filter = request.GET.get('tag')
+    rating_filter = request.GET.get('rating')
+
+
+    if search_querry:
+        #Uses django Q to search for both the movie title, and description
+        #Uses __icontains to search case insensitive
+        movies = movies.filter(
+                Q(movie_title__icontains=search_querry) | Q(movie_description__icontains=search_querry)
+                  )
+
+    if rating_filter:
+        #Uses __gte to get te greater than or equal rating
+        movies = movies.filter(movie_rating__gte=rating_filter)
+
+    if category_filter:
+        movies = movies.filter(category_id=category_filter)
+
+    if tag_filter:
+        #Uses the .distinct() querryset modifier to return distinct entries
+        movies = movies.filter(tags=tag_filter).distinct()
+
+
+    context = {
+            "movies": movies,
+            "categories": categories,
+            "tags": tags
+            }
+
+    return render(request, 'catalog/movies_list.html', context)
+
+   
