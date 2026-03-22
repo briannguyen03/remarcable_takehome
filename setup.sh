43e08d6240e5b026fcce0d1b7f6400150b06fb10
Brian <nguyen.brian1403@gmail.com>
2026-03-21 23:38:05 -0700

    synced with remote repo

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "venv" ]; then
+	echo "Creating virtual enviroment..."
+	python -m venv venv || { echo "Failed to create venv"; exit 1; }
+fi
+
+echo "Activating the virtual enviroment..."
+source venv/bin/activate || { echo "Failed to activate venv"; exit 1; }
+
+
+echo "Installing dependencies..."
+pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
+
+
+echo "Starting server..."
+python manage.py runserver
+	
