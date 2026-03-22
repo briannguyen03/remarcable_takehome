1715b053cc25f35851251c2708a38d19e48c9693
Brian <nguyen.brian1403@gmail.com>
2026-03-22 01:11:44 -0700

    refactored view and html template to allow for selecting multiple tags

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,7 @@ from .models import Movie, Category, Tag
 def movie_list(request):
     """
     Movie search and filtering logic. Retreives all movies, categories, and tags, 
-    then matches them to the search_querry, or filters applied using the GET request.
+    then matches them to the search_query, or filters applied using the GET request.
 
     aggregate all of the fields: movies, categories, and tags into a dictionary 'context' for the html to parse
     """
@@ -19,17 +19,17 @@ def movie_list(request):
     tags = Tag.objects.all()
 
 
-    search_querry = request.GET.get('search')
+    search_query = request.GET.get('search')
     category_filter = request.GET.get('category')
-    tag_filter = request.GET.get('tag')
+    tag_filter = request.GET.getlist('tags') #gets the list of tag ids from the url
     rating_filter = request.GET.get('rating')
 
 
-    if search_querry:
+    if search_query:
         #Uses django Q objects to search for both the movie title, and description
-        #Uses icontains to match tittle and description, case insensitive
+        #Uses icontains to match title and description, case insensitive
         movies = movies.filter(
-                Q(movie_title__icontains=search_querry) | Q(movie_description__icontains=search_querry)
+                Q(movie_title__icontains=search_query) | Q(movie_description__icontains=search_query)
                   )
 
     if rating_filter:
@@ -40,14 +40,15 @@ def movie_list(request):
         movies = movies.filter(category_id=category_filter)
 
     if tag_filter:
-        #Uses the .distinct() querryset modifier to return distinct entries, and prevent duplicate results
-        movies = movies.filter(tags=tag_filter).distinct()
+        #Uses the .distiect() queryset modifier to return distinct entries, and prevent duplicate results
+        movies = movies.filter(tags__id__in=tag_filter).distinct()
 
 
     context = {
             "movies": movies,
             "categories": categories,
-            "tags": tags
+            "tags": tags,
+            "selected_tags": tag_filter,
             }
 
     return render(request, 'catalog/movies_list.html', context)
